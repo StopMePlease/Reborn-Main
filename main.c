6e66e0b454ce565a8b47033a3a69edd4d270dd6c
Duc Do <dotrananhduc@gmail.com>
2016-09-08 22:13:04 +0700

    added

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+https://www.cse.buffalo.edu/~hungngo/Vietnamese/self-printing-prog.html
+
+char *program = "#include <stdio.h>%cchar *program = %c%s%c;%cint main()%c{%c printf(program, 10, 34, program, 34, 10, 10, 10, 10, 10, 10);%c    return 0;%c}%c";
+int main()
+{
+    printf(program, 10, 34, program, 34, 10, 10, 10, 10, 10, 10);
+    return 0;
+}
